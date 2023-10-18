69c0d8b10b0d81121fa85d76ea492014c2be6c29
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2023-10-18 03:43:19 -0400

    More fixes

--- App.xaml.cs
+++ App.xaml.cs
@@ -24,12 +24,12 @@ namespace Concursus
             if (System.IO.Directory.Exists(OLD_FOLDER))
                 System.IO.Directory.Delete(OLD_FOLDER, true);
 
-            Utils.RegisterProtocol(Utils.MM_PROTOCOL, "Etrian Odyssey HD Mod Manager");
+            Utils.RegisterProtocol(Utils.MM_PROTOCOL, "Concursus");
             if (e.Args.Length == 1)
                 this.StartupUri = new Uri("/Concursus;component/GBModPrompt.xaml", UriKind.Relative);
             else
             {
-                const string appName = "EtrianOdysseyModManager";
+                const string appName = "Concursus";
                 bool createdNew;
 
                 _mutex = new Mutex(true, appName, out createdNew);
