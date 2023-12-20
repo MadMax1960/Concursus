1ecfed47bc785eb995b2e56be2f4132e6aa46da4
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2023-12-20 18:26:23 -0500

    hi

--- GBModPrompt.xaml.cs
+++ GBModPrompt.xaml.cs
@@ -18,13 +18,13 @@ using SevenZipExtractor;
 
 namespace Concursus
 {
-    /// <summary>
-    /// Interaction logic for GBModPrompt.xaml
-    /// </summary>
-    public partial class GBModPrompt : Window
-    {
+	/// <summary>
+	/// Interaction logic for GBModPrompt.xaml
+	/// </summary>
+	public partial class GBModPrompt : Window
+	{
 
-        static string DESCRIPTION_HTML = @"
+		static string DESCRIPTION_HTML = @"
             <html>
                 <head>
                     <style>
@@ -39,103 +39,103 @@ namespace Concursus
             </html>
         ";
 
-        public GamebananaMod mod;
+		public GamebananaMod mod;
 
-        public GBModPrompt()
-        {
-            new GBModPrompt(null, null).ShowDialog();
-            this.Close();
-        }
+		public GBModPrompt()
+		{
+			new GBModPrompt(null, null).ShowDialog();
+			this.Close();
+		}
 
-        public GBModPrompt(string game_id = null, string mod_id = null)
-        {
-            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            
-            if(game_id == null && mod_id == null)
-            {
-                string[] args = Environment.GetCommandLineArgs();
-                if (args.Length != 2)
-                    return;
+		public GBModPrompt(string game_id = null, string mod_id = null)
+		{
+			this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
 
-                string arg = args[1];
-                if (arg.StartsWith(Utils.MM_PROTOCOL_LINK))
-                    arg = arg.Substring((Utils.MM_PROTOCOL_LINK).Length);
+			if (game_id == null && mod_id == null)
+			{
+				string[] args = Environment.GetCommandLineArgs();
+				if (args.Length != 2)
+					return;
 
-                string[] res = arg.Trim().Replace("\\", "/").Trim('/').Split('_');
-                if (res.Length != 2)
-                    return;
-                game_id = res[0];
-                mod_id = res[1];
-            }
+				string arg = args[1];
+				if (arg.StartsWith(Utils.MM_PROTOCOL_LINK))
+					arg = arg.Substring((Utils.MM_PROTOCOL_LINK).Length);
 
-            mod = GamebananaMod.GetModInfoFromID(game_id, mod_id);
+				string[] res = arg.Trim().Replace("\\", "/").Trim('/').Split('_');
+				if (res.Length != 2)
+					return;
+				game_id = res[0];
+				mod_id = res[1];
+			}
 
-            InitializeComponent();
-            Themes.UpdateForm(Themes.CURRENT_THEME, this);
+			mod = GamebananaMod.GetModInfoFromID(game_id, mod_id);
 
-            if (mod == null)
-            {
-                MessageBox.Show($"Failed getting mod with id {mod_id}!", "Failed getting mod", MessageBoxButton.OK, MessageBoxImage.Error);
-                Environment.Exit(-1);
-            }
+			InitializeComponent();
+			Themes.UpdateForm(Themes.CURRENT_THEME, this);
 
-            if(mod.mod_dir_path == "mods")
-            {
-                MessageBox.Show($"{mod.GameName} path has not been set yet! Please set it in the normal manager first before trying to install mods for it.", "Path not set", MessageBoxButton.OK, MessageBoxImage.Error);
-                Environment.Exit(-1);
-            }
+			if (mod == null)
+			{
+				MessageBox.Show($"Failed getting mod with id {mod_id}!", "Failed getting mod", MessageBoxButton.OK, MessageBoxImage.Error);
+				Environment.Exit(-1);
+			}
 
-            this.Title = $"Download {mod.name} for {mod.GameName}";
-            this.txtGame.Text = mod.GameName;
-            this.txtModName.Text = mod.name;
-            this.txtSubmitter.Text = mod.submitter;
-            this.txtVersion.Text = mod.version;
+			if (mod.mod_dir_path == "mods")
+			{
+				MessageBox.Show($"{mod.GameName} path has not been set yet! Please set it in the normal manager first before trying to install mods for it.", "Path not set", MessageBoxButton.OK, MessageBoxImage.Error);
+				Environment.Exit(-1);
+			}
+
+			this.Title = $"Download {mod.name} for {mod.GameName}";
+			this.txtGame.Text = mod.GameName;
+			this.txtModName.Text = mod.name;
+			this.txtSubmitter.Text = mod.submitter;
+			this.txtVersion.Text = mod.version;
 
 
-            BitmapImage bitmap = new BitmapImage();
-            bitmap.BeginInit();
-            bitmap.UriSource = new Uri(mod.images[0], UriKind.Absolute);
-            bitmap.EndInit();
+			BitmapImage bitmap = new BitmapImage();
+			bitmap.BeginInit();
+			bitmap.UriSource = new Uri(mod.images[0], UriKind.Absolute);
+			bitmap.EndInit();
 
-            image.Source = bitmap;
+			image.Source = bitmap;
 
-            string description = DESCRIPTION_HTML.Replace("[REPLACE]", mod.description);
-            broDescription.NavigateToString(description);
+			string description = DESCRIPTION_HTML.Replace("[REPLACE]", mod.description);
+			broDescription.NavigateToString(description);
 
-            cboFiles.ItemsSource = mod.files;
-            cboFiles.SelectedIndex = 0;
-        }
+			cboFiles.ItemsSource = mod.files;
+			cboFiles.SelectedIndex = 0;
+		}
 
-        private async void btnDownload_Click(object sender, RoutedEventArgs e)
-        {
-            Progress<string> progress = new Progress<string>(e => {
-                txtProgress.Text += $"{e}\n";
-                txtProgress.ScrollToEnd();
-            });
+		private async void btnDownload_Click(object sender, RoutedEventArgs e)
+		{
+			Progress<string> progress = new Progress<string>(e => {
+				txtProgress.Text += $"{e}\n";
+				txtProgress.ScrollToEnd();
+			});
 
-            btnDownload.IsEnabled = false;
-            
-            string dl_link = mod.files[cboFiles.SelectedValue.ToString()].download_link;
-            byte[] data = null;
-            await Task.Run(() =>
-            {
-                data = Utils.Download(dl_link, progress);
-            });
-            if (data == null)
-            {
-                MessageBox.Show($"Failed to download {dl_link}!", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+			btnDownload.IsEnabled = false;
 
-            string output_dir = mod.mod_dir_path;
+			string dl_link = mod.files[cboFiles.SelectedValue.ToString()].download_link;
+			byte[] data = null;
+			await Task.Run(() =>
+			{
+				data = Utils.Download(dl_link, progress);
+			});
+			if (data == null)
+			{
+				MessageBox.Show($"Failed to download {dl_link}!", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
 
-            txtProgress.Text += $"Reading archive...\n";
-            string parent = "";
-            bool found_data_dir = false;
+			string output_dir = mod.mod_dir_path;
+
+			txtProgress.Text += $"Reading archive...\n";
+			string parent = "";
+			bool found_data_dir = false;
 			bool foundCBBFile = false;
 			using (MemoryStream stream = new MemoryStream(data))
-            using(ArchiveFile archiveFile = new ArchiveFile(stream))
-            {
+			using (ArchiveFile archiveFile = new ArchiveFile(stream))
+			{
 				foreach (var entry in archiveFile.Entries)
 				{
 					List<string> split = entry.FileName.Split(new char[] { '\\', '/' }).ToList();
@@ -157,27 +157,29 @@ namespace Concursus
 						txtProgress.Text += $"Found a .cbb file! Handling it separately...\n";
 					}
 					if (idx != -1)
-                    {
-                        if(idx == 0) // If the data folder is the parent folder in the archive, then make a new folder manually
-                        {
-                            txtProgress.Text += $"{mod.GameFolderDataName} does not have a parent! Creating new parent name...\n";
-                            parent = mod.GetValidFolderName();
-                            output_dir = System.IO.Path.Combine(output_dir, mod.GetValidFolderName());
-                            found_data_dir = true;
-                            break;
-                        } else if(idx == 1)
-                        {
-                            txtProgress.Text += $"{mod.GameFolderDataName} has a parent! Storing parent name...\n";
-                            parent = split[idx - 1];
-                            found_data_dir = true;
-                            break;
-                        } else
-                        {
-                            MessageBox.Show("Archive has a invalid structure! Aborting the operation.", "Invalid Structure", MessageBoxButton.OK, MessageBoxImage.Error);
-                            this.Close();
-                        }
-                    }
-                }
+					{
+						if (idx == 0) // If the data folder is the parent folder in the archive, then make a new folder manually
+						{
+							txtProgress.Text += $"{mod.GameFolderDataName} does not have a parent! Creating new parent name...\n";
+							parent = mod.GetValidFolderName();
+							output_dir = System.IO.Path.Combine(output_dir, mod.GetValidFolderName());
+							found_data_dir = true;
+							break;
+						}
+						else if (idx == 1)
+						{
+							txtProgress.Text += $"{mod.GameFolderDataName} has a parent! Storing parent name...\n";
+							parent = split[idx - 1];
+							found_data_dir = true;
+							break;
+						}
+						else
+						{
+							MessageBox.Show("Archive has a invalid structure! Aborting the operation.", "Invalid Structure", MessageBoxButton.OK, MessageBoxImage.Error);
+							this.Close();
+						}
+					}
+				}
 
 				// Existing code for handling the case when mod.GameFolderDataName is not found
 				// Replace it with the following code
@@ -188,18 +190,18 @@ namespace Concursus
 				}
 
 				archiveFile.Extract(output_dir);
-            }
+			}
 
-            string config_dir = System.IO.Path.Combine(mod.mod_dir_path, parent);
-            if (!File.Exists(System.IO.Path.Combine(config_dir, ModConfig.CONFIG_FILE)))
-            {
-                txtProgress.Text += $"Config not found! Generating new one based on mod data....\n";
-                mod.GetModConfig().SaveToPath(config_dir);
-            }
-            txtProgress.Text += $"Finished downloading {mod.name}! Closing the window in 5 seconds...\n";
-            await Task.Delay(TimeSpan.FromSeconds(5));
-            this.Close();
-        }
+			string config_dir = System.IO.Path.Combine(mod.mod_dir_path, parent);
+			if (!File.Exists(System.IO.Path.Combine(config_dir, ModConfig.CONFIG_FILE)))
+			{
+				txtProgress.Text += $"Config not found! Generating new one based on mod data....\n";
+				mod.GetModConfig().SaveToPath(config_dir);
+			}
+			txtProgress.Text += $"Finished downloading {mod.name}! Closing the window in 5 seconds...\n";
+			await Task.Delay(TimeSpan.FromSeconds(5));
+			this.Close();
+		}
 		private void toggleImage_Click(object sender, RoutedEventArgs e)
 		{
 			if (toggleImage.IsChecked == true)
